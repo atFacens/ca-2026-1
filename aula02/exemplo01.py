81e675566bd1823756122d81cb752d5225f3fd9d
Emerson Paduan <emerson@paduan.dev.br>
2026-03-13 18:57:31 -0300

    aula 02

---
+++ aula02/exemplo01.py
@@ -0,0 +1,27 @@
+# Este é um comentário
+
+# Primeiro programa em Python
+# data: 06/03/2026
+# autor: Emerson
+
+# Entrada --> Processamento --> Saída
+
+print("Boa noite") # apresenta o texto "Boa noite" na tela
+print('Boa noite!')
+
+print('12 + 3')
+print(12 + 3)
+
+print('Resposta', 10 - 4)
+print('Resposta', '=', 3 + 2, '.')
+print('Davi', 'Celso', 'Luiz', 'Ana')
+print('Davi', 'Celso', 'Luiz', 'Ana', sep=',')
+print('Davi', 'Celso', 'Luiz', 'Ana', sep=',', end='.\n')
+print('Davi', 'Celso', 'Luiz', 'Ana', sep='\n')
+
+print('Boa noite!\nprof Emerson aqui.\nHoje está calor')
+
+#     *
+#    **
+#    ***
+#   ****
